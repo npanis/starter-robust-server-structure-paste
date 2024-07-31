cce4749f55797eeafec66b645adc52379e001313
Neriza Panis <neriza.montes@gmail.com>
2024-07-31 16:41:40 +0900

    Added solution

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
+const pastes = require("./data/pastes-data"); // Reads, executes, and returns the exports object from the ./data/pastes-data file, assigning it to a variable
 
-// TODO: Follow instructions in the checkpoint to implement ths API. Adding comment
+//Get specific data per id
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId}  = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  console.log(pasteId, foundPaste);
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
+//Defines a handler for the /pastes path
+app.use("/pastes", (req,res) => { 
+  res.json( { data: pastes }); //Defines a handler for the /pastes path
+});
+
+
 
 // Not found handler
 app.use((request, response, next) => {
